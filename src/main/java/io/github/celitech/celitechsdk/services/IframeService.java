2adc41815aa4e7980cf474b0a3216819e7302840
Celitech <80042395+zgheibali@users.noreply.github.com>
2025-02-04 12:08:51 +0000

    liblab SDK update for version v1.3.36

---
+++ src/main/java/io/github/celitech/celitechsdk/services/IframeService.java
@@ -0,0 +1,51 @@
+package io.github.celitech.celitechsdk.services;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import io.github.celitech.celitechsdk.exceptions.ApiException;
+import io.github.celitech.celitechsdk.http.HttpMethod;
+import io.github.celitech.celitechsdk.http.ModelConverter;
+import io.github.celitech.celitechsdk.http.util.RequestBuilder;
+import io.github.celitech.celitechsdk.models.TokenOkResponse;
+import java.util.concurrent.CompletableFuture;
+import lombok.NonNull;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * IframeService Service
+ */
+public class IframeService extends BaseService {
+
+  public IframeService(@NonNull OkHttpClient httpClient, String serverUrl) {
+    super(httpClient, serverUrl);
+  }
+
+  /**
+   * Generate Token
+   *
+   * @return response of {@code TokenOkResponse}
+   */
+  public TokenOkResponse token() throws ApiException {
+    Request request = this.buildTokenRequest();
+    Response response = this.execute(request);
+    return ModelConverter.convert(response, new TypeReference<TokenOkResponse>() {});
+  }
+
+  /**
+   * Generate Token
+   *
+   * @return response of {@code CompletableFuture<TokenOkResponse>}
+   */
+  public CompletableFuture<TokenOkResponse> tokenAsync() throws ApiException {
+    Request request = this.buildTokenRequest();
+    CompletableFuture<Response> futureResponse = this.executeAsync(request);
+    return futureResponse.thenApplyAsync(response ->
+      ModelConverter.convert(response, new TypeReference<TokenOkResponse>() {})
+    );
+  }
+
+  private Request buildTokenRequest() {
+    return new RequestBuilder(HttpMethod.POST, this.serverUrl, "iframe/token").build();
+  }
+}
