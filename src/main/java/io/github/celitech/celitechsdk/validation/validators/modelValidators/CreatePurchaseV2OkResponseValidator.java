40d1b9508bb66f9e688542d9c82aa9512b39da6e
Celitech <80042395+zgheibali@users.noreply.github.com>
2025-03-14 18:07:29 +0000

    liblab SDK update for version v1.3.42

---
+++ src/main/java/io/github/celitech/celitechsdk/validation/validators/modelValidators/CreatePurchaseV2OkResponseValidator.java
@@ -0,0 +1,25 @@
+package io.github.celitech.celitechsdk.validation.validators.modelValidators;
+
+import io.github.celitech.celitechsdk.models.CreatePurchaseV2OkResponse;
+import io.github.celitech.celitechsdk.validation.Violation;
+import io.github.celitech.celitechsdk.validation.ViolationAggregator;
+
+public class CreatePurchaseV2OkResponseValidator extends AbstractModelValidator<CreatePurchaseV2OkResponse> {
+
+  public CreatePurchaseV2OkResponseValidator(String fieldName) {
+    super(fieldName);
+  }
+
+  public CreatePurchaseV2OkResponseValidator() {}
+
+  @Override
+  protected Violation[] validateModel(CreatePurchaseV2OkResponse createPurchaseV2OkResponse) {
+    return new ViolationAggregator()
+      .add(
+        new CreatePurchaseV2OkResponseProfileValidator("profile")
+          .optional()
+          .validate(createPurchaseV2OkResponse.getProfile())
+      )
+      .aggregate();
+  }
+}
